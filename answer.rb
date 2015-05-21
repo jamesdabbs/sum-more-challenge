38872938e69870111c81825afbb78c5708a68ec4
James Dabbs <jamesdabbs@gmail.com>
2015-05-21 10:40:03 -0400

    Solutions for 4

--- answer.rb
+++ answer.rb
@@ -26,9 +26,13 @@ end
 
 puts "Sum of file: #{running_total}"
 
+
 # 3. how many numbers in this file
 
-ns = 0
+all_tokens = lines.map { |l| l.split(",") }.flatten
+all_numbers = all_tokens.map { |t| t.to_i }
+
+number_of_numbers = 0
 lines.each do |i|
   number_of_numbers += i.split(",").count
 end
@@ -36,4 +40,31 @@ end
 # Other functional options
 n2 = lines.map { |l| l.split(",") }.flatten.count
 n3 = lines.map { |l| l.split(",").count }.reduce :+
+n4 = all_numbers.count
+
+
+# 4. how many are even? divisible by 7?
+
+evens = []
+all_numbers.each do |n|
+  if n.even?
+    evens.push n
+  end
+end
+
+# evens.count => 102 even numbers
+
+sevens = []
+all_numbers.each do |n|
+  if n % 7 == 0
+    sevens.push n
+  end
+end
+
+# sevens.count => 28
+
+# Functionally -
+all_numbers.select { |n| n.even? }.count
+all_numbers.select { |n| n % 7 == 0 }.count
+
 binding.pry
